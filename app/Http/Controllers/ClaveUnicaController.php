16733baba0e85f5dd068b932369b551c3b1647d0
AquaroTorres <alvarotorres@gmail.com>
2023-06-29 14:44:32 -0300

    CU test

--- app/Http/Controllers/ClaveUnicaController.php
+++ app/Http/Controllers/ClaveUnicaController.php
@@ -19,15 +19,12 @@ class ClaveUnicaController extends Controller
     *
     * @return \Illuminate\Http\Response
     */
-    public function login($token = null)
+    public function login($token)
     {
         if($token) {
             $url_base = "https://www.claveunica.gob.cl/openid/userinfo";
             $response = Http::withToken($token)->post($url_base);
             return $response;
-        }
-        else {
-            echo "No se ha proporcionado un access_token";
         }
     }
 
