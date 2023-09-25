a13be55c7bb3a188ee0927627a26fc4f4965dc00
AquaroTorres <alvarotorres@gmail.com>
2023-09-25 10:31:37 -0300

    Code Format

--- app/Http/Controllers/RayenUrgenciaController.php
+++ app/Http/Controllers/RayenUrgenciaController.php
@@ -16,63 +16,63 @@ class RayenUrgenciaController extends Controller
      */
     public function getStatus(Request $request)
     {
-		date_default_timezone_set('America/Santiago');
+        date_default_timezone_set('America/Santiago');
 
-		$date=date('Ymd');
-		$url_base = 'https://api.saludenred.cl/api/healthCareCenter/';
-		$url_query = '/emergencyAdmissions?fromDate=';
+        $date=date('Ymd');
+        $url_base = 'https://api.saludenred.cl/api/healthCareCenter/';
+        $url_query = '/emergencyAdmissions?fromDate=';
 
-		/**
-		 * En archivo .env
-		 * ESTABLECIMIENTOS='{"SAPU Aguirre":{"id":4198,"token":"SDX-TICKET ..."},"SAR Sur":{"id":4162,"token":"SDX-TICKET ..."}}'
-		 */
+        /**
+         * En archivo .env
+         * ESTABLECIMIENTOS='{"SAPU Aguirre":{"id":4198,"token":"SDX-TICKET ..."},"SAR Sur":{"id":4162,"token":"SDX-TICKET ..."}}'
+         */
 
-		$establecimientos = json_decode(env('ESTABLECIMIENTOS'), true);
+        $establecimientos = json_decode(env('ESTABLECIMIENTOS'), true);
 
-		if ($establecimientos === null) {
-			// deal with error...
-			die('No se encuentra la variable ESTABLECIMIENTOS en el .env');
-		}
+        if ($establecimientos === null) {
+            // deal with error...
+            die('No se encuentra la variable ESTABLECIMIENTOS en el .env');
+        }
 
-		foreach ($establecimientos as $nombre => $valores) {
-			$client = new Client(['headers' => [ 'Authorization' => $valores['token']]]);
-			try {
+        foreach ($establecimientos as $nombre => $valores) {
+            $client = new Client(['headers' => [ 'Authorization' => $valores['token']]]);
+            try {
 
-				// $response = $client->get('https://i.saludiquique.cl/dev/get-ip',['http_errors' => false]);
-				// die('listoco');
-				$response = $client->get($url_base . $valores['id'] . $url_query . $date,['http_errors' => false]);
+                // $response = $client->get('https://i.saludiquique.cl/dev/get-ip',['http_errors' => false]);
+                // die('listoco');
+                $response = $client->get($url_base . $valores['id'] . $url_query . $date,['http_errors' => false]);
 
-				if($response->getStatusCode() == 200) {
-					$array = array_count_values(array_column(json_decode($response->getBody(),true),'AdmissionStatus'));
+                if($response->getStatusCode() == 200) {
+                    $array = array_count_values(array_column(json_decode($response->getBody(),true),'AdmissionStatus'));
 
-					$count['data'][$nombre]['En espera'] = 0;
-					$count['data'][$nombre]['En box'] = 0; //12, 99, 100
+                    $count['data'][$nombre]['En espera'] = 0;
+                    $count['data'][$nombre]['En box'] = 0; //12, 99, 100
 
-					if(isset($array[1])) {
-							$count['data'][$nombre]['En espera'] = $array[1];
-					}
+                    if(isset($array[1])) {
+                            $count['data'][$nombre]['En espera'] = $array[1];
+                    }
 
-					if(isset($array[12])) {
-							$count['data'][$nombre]['En box'] += $array[12];
-					}
-					if(isset($array[99])) {
-							$count['data'][$nombre]['En box'] += $array[99];
-					}
-					if(isset($array[100])) {
-							$count['data'][$nombre]['En box'] += $array[100];
-					}
-				}
-				else {
-					$count['data'][$nombre]['En espera'] = 'Error';
-					$count['data'][$nombre]['En box'] = 'Error';
-				}
-			} catch(RequestException $e) {
-				die('Tiempo de espera agotado');
-			}
+                    if(isset($array[12])) {
+                            $count['data'][$nombre]['En box'] += $array[12];
+                    }
+                    if(isset($array[99])) {
+                            $count['data'][$nombre]['En box'] += $array[99];
+                    }
+                    if(isset($array[100])) {
+                            $count['data'][$nombre]['En box'] += $array[100];
+                    }
+                }
+                else {
+                    $count['data'][$nombre]['En espera'] = 'Error';
+                    $count['data'][$nombre]['En box'] = 'Error';
+                }
+            } catch(RequestException $e) {
+                die('Tiempo de espera agotado');
+            }
 
-		}
-		$count['updated'] = date('Y-m-d H:i');
+        }
+        $count['updated'] = date('Y-m-d H:i');
 
-		return isset($count) ? response()->json($count) : null;
+        return isset($count) ? response()->json($count) : null;
     }
 }
